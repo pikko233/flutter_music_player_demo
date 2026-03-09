30eeaa4ac61316d5324a2c2c7aaabceb5479ccd0
pikko <lzs32486@gmail.com>
2026-03-09 21:48:41 +0800

    feat: 完成侧边栏drawer

--- lib/viewmodel/splash_view_model.dart
+++ lib/viewmodel/splash_view_model.dart
@@ -1,9 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_music_player_demo/view/main_tabview/main_tabview.dart';
 import 'package:get/get.dart';
 
 class SplashViewModel extends GetxController {
+  var scaffoldKey = GlobalKey<ScaffoldState>();
+
   void loadView() async {
     await Future.delayed(const Duration(seconds: 2));
     Get.to(() => const MainTabView());
+  }
+
+  void openDrawer() {
+    scaffoldKey.currentState?.openDrawer();
+  }
+
+  void closeDrawer() {
+    scaffoldKey.currentState?.closeDrawer();
   }
 }
